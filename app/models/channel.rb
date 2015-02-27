79614fde854e620af99c39f45308be49c9ad492c
Max Trubetskoy <handcrab@gmail.com>
2015-02-27 07:51:54 +0300

    add channel validations

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,3 +1,27 @@
 class Channel < ActiveRecord::Base
   belongs_to :user
+
+  validates :source_url, presence: true
+  validate :source_url, :is_uri_valid
+
+  before_save :generate_title
+
+  private
+  def generate_title
+    return title unless title.strip.empty?      
+    url = URI source_url
+    begin
+      str = URI.unescape url.query
+      str = str.scan(/[^&;]+?=([^&;]*)/).join('; ').mb_chars.titleize.to_s        
+    rescue
+      str = url.host
+    end
+    self.update title: str
+  end
+
+  def is_uri_valid
+      URI source_url    
+    rescue
+      errors.add(:source_url, "Invalid URI")
+  end
 end
