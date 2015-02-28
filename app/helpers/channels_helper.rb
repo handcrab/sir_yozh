fbc3f4c2759ccb9f13340c2a48f69dceba9a142f
Max Trubetskoy <handcrab@gmail.com>
2015-02-28 14:52:29 +0300

    add styling on tags

--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,3 +1,18 @@
 module ChannelsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def link_to_edit channel
+    link_to edit_channel_path channel do
+      content_tag :i, '', class: 'mdi-content-create'
+      #| Edit      
+    end
+  end
+
+  def link_to_delete channel
+    link_to channel, data: {confirm: 'Are you sure?'}, method: :delete do
+      #| Destroy
+      content_tag :i, '', class: 'mdi-content-clear'
+    end
+  end
+
 end
